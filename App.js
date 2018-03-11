22b5fb38b53280a96eee932a0f7911a27416279a
github username <github email address>
2018-03-11 14:11:40 -0700

    added firebase

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+
+import { Header } from './src/components/common';
 
 
 class App extends Component {
+
+	componentWillMount() {
+		firebase.initializeApp({
+			apiKey: 'AIzaSyB69rmKDxO8mkrQSi0QZ5m1r6KZMMJ_tvU',
+			authDomain: 'auth-c6eb6.firebaseapp.com',
+			databaseURL: 'https://auth-c6eb6.firebaseio.com',
+			projectId: 'auth-c6eb6',
+			storageBucket: 'auth-c6eb6.appspot.com',
+			messagingSenderId: '509356218529'
+		  });
+	}
+
 	render() {
 		return (
 			<View>
+				<Header headerText="Authentication" />
 				<Text>An App</Text>
 			</View>
 		);
--- src/components/common/CardItem.js
+++ src/components/common/CardItem.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { View } from 'react-native';
 
 const CardItem = (props) => {
-    return (
-        <View style={styles.containerStyle}>
-            {props.children}
-        </View>
-    );
+	return (
+		<View style={styles.containerStyle}>
+			{props.children}
+		</View>
+	);
 };
 
 const styles = {
-    containerStyle: {
-        borderBottomWidth: 1,
-        padding: 5,
-        backgroundColor: '#fff',
-        justifyContent: 'flex-start',
-        flexDirection: 'row',
-        borderColor: '#ddd',
-        position: 'relative'
-    }
+	containerStyle: {
+		borderBottomWidth: 1,
+		padding: 5,
+		backgroundColor: '#fff',
+		justifyContent: 'flex-start',
+		flexDirection: 'row',
+		borderColor: '#ddd',
+		position: 'relative'
+	}
 }
-export default CardItem;
+export { CardItem };
