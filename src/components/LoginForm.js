29e46f599df66274d2e7fe010617acd8bed4ab5b
github username <github email address>
2018-03-12 21:43:03 -0700

    added Spinner component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,18 +7,23 @@ import { Button, Card, CardItem, Input } from './common';
 class LoginForm extends Component {
     state = { email: '', password: '', error: '' };
 
+    //when the login button is pressed, it fires off this helper
+    //method. this helper method receives an email and a password
+    //it then sends it down the authentication flow. if no login is 
+    //found, authentication error comes up. 
     onButtonPress() {
         const { email, password } = this.state;
+        //this clears authentication error if user
+        //enters login info again
+        this.setState({ error: ''});
         //this statement returns a promise
         firebase.auth().signInWithEmailAndPassword(email, password)
             //catch statement, says if request fails then enter this function
-
             .catch(() => {
                 //asynchronous method. also returns a promise
                 firebase.auth().createUserWithEmailAndPassword(email, password)
                     .catch(() => {
                         this.setState({ error: 'Authentication Failed.'});
-
                     }); 
             });
     }
