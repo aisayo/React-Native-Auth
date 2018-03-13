5e8dc7344fbc0b83065c734b52ea84dab9e34c47
github username <github email address>
2018-03-12 21:49:31 -0700

    added style to spinner

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
 
 const Spinner = () => {
-    return (
-        <View />
-    );
+	return (
+		<View style={styles.spinnerStyle}>
+            <ActivityIndicator />
+        </View>
+	);
+};
+
+const styles = {
+    spinnerStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 }
 
 
